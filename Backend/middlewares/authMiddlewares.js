cdda4ef0d3e3e3018e75cb12dd21de7d0204018c
juvedibuar00 <caiomete2020@gmail.com>
2025-01-11 14:15:09 -0300

    autencaçao por middleware

--- Backend/middlewares/authMiddlewares.js
+++ Backend/middlewares/authMiddlewares.js
@@ -2,15 +2,15 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.header.authorization;
-    if (!authHeader) return res.status(401).json({message:'Token não fornecido'});
+    if (!authHeader) return res.status(401).json({ message: 'Token não fornecido' });
 
     const token = authHeader.split(' ')[1];
-    try{
+    try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch {
-        return res.status(401).json({message:'Token inválido'});
+        return res.status(401).json({ message: 'Token inválido' });
     }
 };
 
