c1c7f34beeca1d3a16c30bd7ae45695b46ad1dce
unknown <camillolmcastro@gmail.com>
2025-01-25 15:18:37 -0300

    projeto atualizado

--- Frontend/src/components/login/cadastroUsuario/cadastroUsuario.jsx
+++ Frontend/src/components/login/cadastroUsuario/cadastroUsuario.jsx
@@ -1,28 +1,88 @@
 import './cadastroUsuario.css';
+// Importações
+import { useState } from 'react';
+import axios from 'axios';
 
 function CadastroUsuario() {
+    // uteis para coletar dados do usuario
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+//    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Configuração dos useStates
+        const dadosCadastro = {
+            nome: name,
+            email: email,
+            senha: password,
+        };
+        axios.post('http://localhost:3001/autth/register', dadosCadastro)
+        .then(Response => {
+            // console.log(Response.data);
+            alert('Usuário Cadastrado com Sucesso!!!');
+            setName('');
+            setEmail('');
+            setPassword('');
+        })
+        .catch(error => {
+            console.error('Erro ao Cadastrar Usuário:', error);
+            // seError(error.response?.data || 'Erro desconhecido')
+        });
+
+    
+    };
+
+
     return (
         <div className="cadastro-container">
             <div className="header">
                 <h1>Cadastro de Usuário</h1>
             </div>
             <div className="form-container">
-                <div className="input-group">
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" placeholder="Digite seu e-mail" />
-                </div>
+                
                 <div className="input-group">
                     <label htmlFor="fullName">Nome Completo</label>
-                    <input type="text" id="fullName" placeholder="Digite seu nome completo" />
+                    <input name='name' type="text" id="fullName" placeholder="Digite seu nome completo"
+                    onChange={(e) => setName (e.target.value)}
+                    value={name}
+                    required 
+                    />
+                
+
+                <div className="input-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input name='email' type="email" id="email" placeholder="Digite seu e-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required 
+                    
+                    />
+                </div>
+
+                
+
+
                 </div>
                 <div className="input-group">
                     <label htmlFor="password">Senha</label>
-                    <input type="password" id="password" placeholder="Digite sua senha" />
+                    <input name='password' type="password" id="password" 
+                    placeholder="Digite sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                    
+                    />
                 </div>
+
+
                 <div className="input-group">
                     <label htmlFor="confirmPassword">Confirme a Senha</label>
                     <input type="password" id="confirmPassword" placeholder="Confirme sua senha" />
                 </div>
+
+
                 <div className="input-group">
                     <label htmlFor="phone">Telefone</label>
                     <input type="tel" id="phone" placeholder="Digite seu telefone" />
